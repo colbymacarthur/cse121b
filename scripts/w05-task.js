76fe1d98d26bf0fd02465331d996c5009f9731ba
colbymacarthur <colby.mac89@gmail.com>
2024-02-08 21:40:13 -0600

    week 5

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -5,22 +5,22 @@ const templesElement = document.getElementById('temples')
 let templeList ={}
 
 /* async displayTemples Function */
-let displayTemples = templeList => {
-    templeList.forEach(temple => {
-        const article = document.createElement('article');
+const displayTemples = (temples) => {
+    temples.forEach (temple => {
 
-        const h3 = document.createElement('h3')
-        h3.textContent = temple.templeName;
-        
-        const img = document.createElement('image')
-        img.src = temple.imageUrl
-        img.alt = temple.location;
+    let article = document.createElement('article');
 
-        article.appendChild(h3);
-        article.appendChild(img);
+    let h3 = document.createElement('h3');
+    h3.textContent = temple.templeName;
 
-        templesElement.appendChild(article);
+    let img = document.createElement('img');
+    img.src = temple.imageUrl;
+    img.alt = temple.location;
 
+    article.appendChild(h3);
+    article.appendChild(img);
+
+    templesElement.appendChild(article);
     });
 }
 
@@ -35,28 +35,28 @@ const getTemples = async() => {
 
 /* reset Function */
 const reset = () => {
-    templesElement.innerHTML = "";
+    templesElement.innerHTML = '';
 }
 
 /* filterTemples Function */
 const filterTemples = (temples) => {
     reset();
-    let filter = document.getElementById('filtered')
+    let filter = document.getElementById('filtered').value;
     switch (filter) {
-        case "utah":
-            displayTemples(temples.filter(temple => temple.location.toLowerCase().includes("utah")));
+        case 'utah':
+            displayTemples(temples.filter(temple => temple.location.includes('Utah')));
             break;
 
-        case "notutah":
-            displayTemples(temples.filter(temple => !temple.location.toLowerCase().includes("utah")));
+        case 'notutah':
+            displayTemples(temples.filter(temple => !temple.location.toLowerCase().includes('utah')));
             break;
 
-        case "older":
-            displayTemples(temples.filter(temple => new Date(temple.dedicated) < new Date(1950, 0, 1)));
+        case 'older':
+            displayTemples(temples.filter(temple => parseInt(temple.dedicated.split(",")[0]) < 1950));
             break;
 
-        case "all":
-            displayTemples(temples);
+        case 'all':
+            displayTemples(temples);         
             break;
 
         default:
@@ -65,7 +65,6 @@ const filterTemples = (temples) => {
 }
 
 
-
 /* Event Listener */
-document.querySelector("#filtered").addEventListener("change", () => { filterTemples(templeList) });
+document.getElementById('filtered').addEventListener('change', () => { filterTemples(templeList) });
 getTemples();
